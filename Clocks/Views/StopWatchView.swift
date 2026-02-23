f7d89fe39140be8987b7ec55e6771960d14b2ea6
Alexander Saadia <asaadiajinich@lcs.on.ca>
2026-02-23 14:39:20 -0500

    Step 1, idk

---
+++ Clocks/Views/StopWatchView.swift
@@ -0,0 +1,18 @@
+//
+//  StopWatchView.swift
+//  Clocks
+//
+//  Created by Alexander Saadia on 23/02/26.
+//
+
+import SwiftUI
+
+struct StopWatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StopWatchView()
+}
---
+++ Clocks/Views/WorldClockView.swift
@@ -0,0 +1,18 @@
+//
+//  WorldClockView.swift
+//  Clocks
+//
+//  Created by Alexander Saadia on 23/02/26.
+//
+
+import SwiftUI
+
+struct WorldClockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorldClockView()
+}
